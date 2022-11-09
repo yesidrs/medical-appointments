bae7e451348a00d7719a2a3137f2ee25567886d8
yesiddev <yesiddeveloper@gmail.com>
2022-11-08 23:10:40 -0500

    add useState and initial form validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react'
+
 const Form = () => {
+  const [pet, setPet] = useState({
+    name: '',
+    owner: '',
+    email: '',
+    discharge: '',
+    syntoms: ''
+  })
+
+  let { name, owner, email, discharge, syntoms } = pet
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target
+    setPet({ ...pet, [name]: value })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (validateForm()) {
+      console.log('Button pressed')
+    }
+  }
+
+  const validateForm = () => {
+    if ([name, owner, email, discharge, syntoms].includes('')) {
+      console.log('All fields are required')
+      return false
+    }
+
+    return true
+  }
+
   return (
     <div className="w-11/12 md:w-1/2 lg:w-2/5 mb-10">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 mb-10 text-center">
         Añade Pacientes y <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5">
         <div className="mb-5">
           <label htmlFor="pet" className="block text-gray-700 uppercase font-bold">
             nombre mascota
@@ -13,8 +46,11 @@ const Form = () => {
           <input
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="pet"
+            name="name"
             placeholder="Nombre de la Mascota"
             type="text"
+            onChange={handleChange}
+            value={name}
           />
         </div>
         <div className="mb-5">
@@ -24,8 +60,11 @@ const Form = () => {
           <input
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="owner"
+            name="owner"
             placeholder="Nombre del Propietario"
             type="text"
+            onChange={handleChange}
+            value={owner}
           />
         </div>
         <div className="mb-5">
@@ -35,8 +74,11 @@ const Form = () => {
           <input
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="email"
+            name="email"
             placeholder="Email Contacto Propietario"
             type="email"
+            onChange={handleChange}
+            value={email}
           />
         </div>
         <div className="mb-5">
@@ -46,7 +88,10 @@ const Form = () => {
           <input
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="discharge"
+            name="discharge"
             type="date"
+            onChange={handleChange}
+            value={discharge}
           />
         </div>
         <div className="mb-5">
@@ -55,9 +100,12 @@ const Form = () => {
           </label>
           <textarea
             id="symptoms"
+            name="syntoms"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Describe los Síntomas"
-          ></textarea>
+            onChange={handleChange}
+            value={syntoms}
+          />
         </div>
         <button
           type="submit"
