f8fcfb9f48e1c351581e89dadffd66bd876fcf25
yesiddev <yesiddeveloper@gmail.com>
2022-11-11 20:58:13 -0500

    add array object state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
 import Form from './components/Form'
 import Header from './components/Header'
 import Patients from './components/Patients'
 
+import { IPet } from './types/pet'
+
 function App() {
+  const [pets, setPets] = useState<IPet[]>([])
+
   return (
     <div className="container mx-auto mt-12">
       <Header />
       <div className="mt-12 flex flex-col max-sm:items-center md:flex-row">
-        <Form />
-        <Patients />
+        <Form setPets={setPets} />
+        <Patients pets={pets} />
       </div>
     </div>
   )
