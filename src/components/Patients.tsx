4b46599a222f37d6b392f0a0a06e2b8e6b73ed06
yesiddev <yesiddeveloper@gmail.com>
2022-11-16 13:04:43 -0500

    add dynamic title

--- src/components/Patients.tsx
+++ src/components/Patients.tsx
@@ -4,10 +4,22 @@ import Patient from './Patient'
 const Patients = ({ pets }: { pets: IPet[] }) => {
   return (
     <div className="w-11/12 md:w-1/2 lg:w-3/5 md:h-screen overflow-y-auto">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus <span className="font-bold text-indigo-600">Pacientes y Citas</span>
-      </p>
+      {pets && pets.length > 0 ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Administra tus <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No Hay Pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Los Pacientes Agregados{' '}
+            <span className="font-bold text-indigo-600">aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
       {pets.map((pet: IPet) => (
         <Patient pet={pet} key={pet.id} />
       ))}
