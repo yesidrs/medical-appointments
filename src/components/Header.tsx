0d35703fa48fdd37762cb3b535ca1f1e7deea4a1
yesiddev <yesiddeveloper@gmail.com>
2022-11-23 23:22:46 -0500

    change title text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-      Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span>
+      Medical Follow-up <span className="text-indigo-600">Pets</span>
     </h1>
   )
 }
